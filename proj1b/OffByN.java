06983efd47076bd232c21ee6ce8a811f0ce9c09b
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2021-09-13 10:00:44 +0200

    added override tag

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -5,6 +5,7 @@ public class OffByN implements CharacterComparator {
         this.N = N;
     }
 
+    @Override
     public boolean equalChars(char x, char y) {
         if (N == Math.abs(x - y)) {
             return true;
